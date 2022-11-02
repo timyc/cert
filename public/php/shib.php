2d760420a179300652d391ca33a90b515cbe01a7
Tim Chen <29265905+timyc@users.noreply.github.com>
2022-11-02 11:54:21 -0700

    minor callback script changes

--- public/php/shib.php
+++ public/php/shib.php
@@ -14,13 +14,14 @@ echo 'Processing your request...';
 parse_str($_SERVER['QUERY_STRING'], $query);
 // Contains the JWT
 $token = $query['token'];
+$authorization = "Authorization: Bearer $token";
 // Contains the university secret key and user ID verified by the university
 $identifier = $_SERVER['HTTP_IDENTIFIER'];
 // Initiate the cURL session
 $req = curl_init(API_INIT);
+curl_setopt($req, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization));
 curl_setopt($req, CURLOPT_POST, true);
 curl_setopt($req, CURLOPT_POSTFIELDS, array(
-    'token' => $token,
     'identifier' => $identifier
 ));
 curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
@@ -36,14 +37,16 @@ if ($res['status'] === 'success') {
     $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
     if ($socket === false) {
         echo 'socket_create() failed: reason: ' . socket_strerror(socket_last_error()) . '';
+        header('Location: ' . HOME_PAGE . '/error');
         return;
     }
     // Connect to the server
-    $address = json_decode($res['msg']['json'])->sIP;
-    $port = json_decode($res['msg']['json'])->sPort;
+    $address = json_decode($res['msg']['json'])['sIP'];
+    $port = json_decode($res['msg']['json'])['sPort'];
     $result = socket_connect($socket, $address, $port);
     if ($result === false) {
         echo 'socket_connect() failed: reason: ' . socket_strerror(socket_last_error($socket)) . '';
+        header('Location: ' . HOME_PAGE . '/error');
         return;
     }
     /*
